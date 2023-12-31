cc83a82522630050970492f2fad304bc1ed32730
Nikhil Nair <niknair31898@gmail.com>
2023-12-31 14:13:02 -0500

    Updated deploy script to update lambda too

--- deploy.ps1
+++ deploy.ps1
@@ -1,13 +1,3 @@
-# Function to check if Docker is logged in to AWS ECR
-function Check-DockerLogin {
-    try {
-        docker info | Out-String -Stream | Select-String -Pattern "ECRLogin"
-        return $true
-    } catch {
-        return $false
-    }
-}
-
 # Function to check if AWS ECR repository exists
 function Check-ECRRepositoryExists {
     param ($repositoryName)
@@ -23,19 +13,54 @@ function Check-ECRRepositoryExists {
     }
 }
 
-# Login to AWS ECR if not logged in
-if (Check-DockerLogin) {
-    aws ecr get-login-password | docker login --username AWS --password-stdin 832214191436.dkr.ecr.ap-south-1.amazonaws.com
-}
-
 # Create repository if it does not exist
 $repositoryName = "mia-lambda"
 if (-not (Check-ECRRepositoryExists $repositoryName)) {
     aws ecr create-repository --repository-name $repositoryName
 }
 
-# Build and push Docker image
+# AWS Login
+aws ecr get-login-password | docker login --username AWS --password-stdin 832214191436.dkr.ecr.ap-south-1.amazonaws.com
+
+# Build Docker image
 docker build -t mia-lambda .
+
+# Tag the image with 'latest'. This tag will overwrite any existing 'latest' image in the repository.
 docker tag mia-lambda:latest 832214191436.dkr.ecr.ap-south-1.amazonaws.com/mia-lambda:latest
+
+# Push the image. This will overwrite the existing 'latest' image in the ECR repository.
 docker push 832214191436.dkr.ecr.ap-south-1.amazonaws.com/mia-lambda:latest
+
+# List images in the repository to confirm the push
 aws ecr list-images --repository-name mia-lambda --region ap-south-1
+
+# Define Lambda function name
+$lambdaFunctionName = "mia-audio"
+
+# Update Lambda function with the new image
+$imageUri = "832214191436.dkr.ecr.ap-south-1.amazonaws.com/mia-lambda:latest"
+aws lambda update-function-code --function-name $lambdaFunctionName --image-uri $imageUri
+
+# Function to check if the Lambda function has been updated with the new image
+function Check-LambdaUpdate {
+    param ($functionName, $expectedUri)
+    $functionInfo = aws lambda get-function --function-name $functionName | ConvertFrom-Json
+    return $functionInfo.Configuration.Code.ImageUri -eq $expectedUri
+}
+
+# Wait for the update to be confirmed
+$tries = 0
+$maxTries = 10
+$updated = $false
+while ($tries -lt $maxTries -and -not $updated) {
+    Write-Host "Checking if Lambda function has been updated... (Attempt: $($tries + 1))"
+    Start-Sleep -Seconds 3
+    $updated = Check-LambdaUpdate -functionName $lambdaFunctionName -expectedUri $imageUri
+    $tries++
+}
+
+if ($updated) {
+    Write-Host "Lambda function updated successfully with the new image."
+} else {
+    Write-Host "Failed to confirm the update of the Lambda function after $($maxTries) attempts."
+}
